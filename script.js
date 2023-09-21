581bc316355bdce0442d837b1fdb3107fd0c7d34
Chirag Mittal <chiragmittal36269@gmail.com>
2023-09-21 17:16:20 +0530

    commit changes

--- script.js
+++ script.js
@@ -122,19 +122,9 @@ searchInput.addEventListener("keyup", (e) => {
 
 const setCategories = () => {
     const allCats = watches.map((item) => item.cat);
-    // console.log(allCats);
     let categories = [
         "All",
         ...allCats.filter((item, index) => {
-            // console.log(allCats);
-            // console.log(`allCats.indexOf(item): ${allCats.indexOf(item)}`);
-            // console.log(`item: ${item}`);
-            // console.log(`index: ${index}`);
-            // console.log(
-            //     `allCats.indexOf(item) === index : ${
-            //         allCats.indexOf(item) === index
-            //     }`
-            // );
             return allCats.indexOf(item) === index; // remove duplicates from the array
         }),
     ];
